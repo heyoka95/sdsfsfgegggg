e9c65913a42e10dcb2c250356c2e9762a13f22c8
heyoka95 <33101550+heyoka95@users.noreply.github.com>
2019-04-19 09:43:58 +0200

    here

--- Start.java
+++ Start.java
@@ -9,24 +9,25 @@ import java.util.Set;
 public class Start {
 
 	static Random r = new Random();
-	//static LinkedHashMap<int[], String> marsKarte; //Die Datenstruktur ist zu aufwedig, es geht viel einfacher mit einem CharArray
 	
-	static char[][] marsKarte; //Änderung der Datenstruktur dient zur Einfachheit
-	static final int WIDTH_MARSES = 80;      		            
-	static final int HEIGHT_MARS = 20;			
-	static int rover_row = HEIGHT_MARS / 2;   
-	static int rover_col = WIDTH_MARSES / 2 + 2;
+	
+	static char[][] marsKarte; 
+	static final int BREITE_MARSES = 80;      		            
+	static final int HOEHE_MARSES = 20;			
+	static int rover_row = HOEHE_MARSES / 2;   
+	static int rover_col = BREITE_MARSES / 2 + 2;
+	
 	static final char up =   '^';
-	static final char down = 'V';    
+	static final char down = 'V';
 	static final char west = '<';
 	static final char east = '>';
+
 	
-	
-	public static void createMarsAndPutRoverinTheMiddle() {
-	marsKarte=new char[HEIGHT_MARS ][WIDTH_MARSES];
+	public static void erstelleKarteUndSetzeRoverInDieMitte() {
+		marsKarte=new char[HOEHE_MARSES][BREITE_MARSES];
 		
-		for (int col = 0; col < WIDTH_MARSES; col++) {
-			for (int row = 0; row < HEIGHT_MARS; row++) {
+		for (int col = 0; col < BREITE_MARSES; col++) {
+			for (int row = 0; row < HOEHE_MARSES; row++) {
 				int[] position = new int[] { col, row };
 				
 				if (r.nextDouble() < 0.25 && !(rover_col == col && rover_row == row)){
@@ -37,12 +38,8 @@ public class Start {
 			}
 		}
 		
-		marsKarte[rover_row][rover_col]= '^';
-	}	
-	
-	
-	
-	
+		marsKarte[rover_row][rover_col]=up;
+	}
 	
 	public static char getPosition(int[] position) {
 		int row=position[1];
@@ -50,16 +47,15 @@ public class Start {
 		return marsKarte[row][col];
 	}
 
-	
 	public static void printField() {
-		for (int row = 0; row < HEIGHT_MARS; row++) {
-			for (int col = 0; col < WIDTH_MARSES; col++) {
+		for (int row = 0; row < HOEHE_MARSES; row++) {
+			for (int col = 0; col < BREITE_MARSES; col++) {
 				System.out.print( getPosition(new int[]{col,row}) );
 			}
 			System.out.println();
 		}
 		
-		for (int i = 0; i < WIDTH_MARSES; i++) {
+		for (int i = 0; i < BREITE_MARSES; i++) {
 			System.out.print("=");
 		}
 		
@@ -74,19 +70,19 @@ public class Start {
 			System.out.println("Seed: " + seed);
 		}
 		System.out.println("erstelle karte");
-		createMarsAndPutRoverinTheMiddle();
+		erstelleKarteUndSetzeRoverInDieMitte();
 		
 		String instructions = args[0];
 		
 		printField();
 		
 		for (int i = 0; i < instructions.length(); i++) {
-			move(instructions.charAt(i));
+			bewegeRover(instructions.charAt(i));
 			printField();
 		}
 	}
-	
-	public static void move(char c) { 
+
+	public static void bewegeRover(char c) {
 		System.out.println("bewege "+c);
 		int[] aktuellePosition = findeRover();
 		int[] newPosition=new int[2];
@@ -116,48 +112,40 @@ public class Start {
 			marsKarte[rover_row][rover_col]=rotate_left(getOrientation());
 		} else if (c == 'r' ) {
 			marsKarte[rover_row][rover_col]=rotate_right(getOrientation());
-		} 
-		
+		}
 	}
 	
-	
-		private static char rotate_left(char orientation){
-			if(orientation==up) return west;
-			if(orientation==west) return down;
-			if(orientation==down) return east;
-			if(orientation==east) return up;
+	private static char rotate_left(char orientation){
+		if(orientation==up) return west;
+		if(orientation==west) return down;
+		if(orientation==down) return east;
+		if(orientation==east) return up;
 		return up;
-		}
-		
-		
-		private static char getOrientation(){
-			return getPosition(new int[]{rover_col,rover_row});
-		}
+	}
 	
-		
-		private static char rotate_right(char orientation){
-			return rotate_left(rotate_left(rotate_left(orientation)));
-		}
-		
+	private static char rotate_right(char orientation){
+		return rotate_left(rotate_left(rotate_left(orientation)));
+	}
 	
-		private static int[] findeRover() {
-			return new int[]{rover_col,rover_row};
-		
-		}
-		
-		private static boolean isFreePosition(int[] position){		//prüft ob die Stelle frei ist
-			return isValidPosition(position) && getPosition(position) == ' ';
-		}
-		
-		private static boolean isValidPosition(int[] position){
-			int row=position[1];
-			int col=position[0];
-			return row>=0 && col >=0 && row < marsKarte.length && col < marsKarte[0].length;
-		}
-		
-		
-		
-		private static int[] getPositionFacing(){    //Die Methode die das Vorwärtsgehen für den Rover zuständig ist
+	private static int[] findeRover() {
+		return new int[]{rover_col,rover_row};
+	}
+	
+	private static char getOrientation(){
+		return getPosition(new int[]{rover_col,rover_row});
+	}
+	
+	private static boolean isValidPosition(int[] position){
+		int row=position[1];
+		int col=position[0];
+		return row>=0 && col >=0 && row < marsKarte.length && col < marsKarte[0].length;
+	}
+	
+	private static boolean isFreePosition(int[] position){
+		return isValidPosition(position) && getPosition(position) == ' ';
+	}
+	
+	private static int[] getPositionFacing(){
 		int col=rover_col;
 		int row=rover_row;
 		char orientation = getOrientation();
@@ -168,7 +156,7 @@ public class Start {
 		return findeRover();
 	}
 	
-	private static int[] getPositionBack(){		////Die Methode die das Rückäwrtsgehen für den Rover zuständig ist
+	private static int[] getPositionBack(){
 		int col=rover_col;
 		int row=rover_row;
 		char orientation = getOrientation();
